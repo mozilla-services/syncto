16990702f84c09a00f52b6c3d3300578e386ed07
Rémy HUBSCHER <rhubscher@mozilla.com>
2015-08-28 12:37:38 +0200

    Add the DELETE record endpoint.

--- syncto/views/record.py
+++ syncto/views/record.py
@@ -30,3 +30,16 @@ def record_get(request):
     convert_headers(sync_client.raw_resp, request.response)
 
     return {'data': record}
+
+
+@record.delete(permission=NO_PERMISSION_REQUIRED)
+def record_delete(request):
+    collection_name = request.matchdict['collection_name']
+    record_id = request.matchdict['record_id']
+    sync_id = uuid4_to_base64(record_id)
+
+    sync_client = build_sync_client(request)
+    sync_client.delete_record(collection_name, sync_id)
+
+    request.response.status_code = 204
+    return None
