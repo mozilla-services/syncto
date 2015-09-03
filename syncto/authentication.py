6109ddc32ce44fc6676662025d179c5004bfab9c
Rémy HUBSCHER <rhubscher@mozilla.com>
2015-09-03 14:39:56 +0200

    Use the HTTPError view handler for authentication errors.

--- syncto/authentication.py
+++ syncto/authentication.py
@@ -40,19 +40,5 @@ def build_sync_client(request):
 
     bid_assertion = authorization_header.split(" ", 1)[1]
     client_state = request.headers[CLIENT_STATE_HEADER]
-    try:
-        sync_client = SyncClient(bid_assertion, client_state)
-    except HTTPError as e:
-        if e.response.status_code in (400, 401):
-            message = '%s %s: %s' % (e.response.status_code,
-                                     e.response.reason,
-                                     e.response.text)
-            response = http_error(httpexceptions.HTTPUnauthorized(),
-                                  errno=ERRORS.INVALID_AUTH_TOKEN,
-                                  message=message)
-            # Forget the current user credentials.
-            response.headers.extend(forget(request))
-            raise response
-        else:
-            raise service_unavailable(e, request)
+    sync_client = SyncClient(bid_assertion, client_state)
     return sync_client
