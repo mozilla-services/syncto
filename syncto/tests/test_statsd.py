ed91e5ea48d6c86c03e8ec1a6930cde2bd57f0c5
Rémy HUBSCHER <rhubscher@mozilla.com>
2015-09-30 16:45:37 +0200

    Statsd tests skeleton.

---
+++ syncto/tests/test_statsd.py
@@ -0,0 +1,9 @@
+from .support import BaseWebTest, unittest
+
+
+class StatsdSyncClientTest(BaseWebTest, unittest.TestCase):
+    def test_statsd_time_token_server_call(self):
+        pass
+
+    def test_statsd_time_sync_client_calls(self):
+        pass
