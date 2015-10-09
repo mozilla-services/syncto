2afe2f80933fbe8118302862a7e88c700963e678
Rémy HUBSCHER <rhubscher@mozilla.com>
2015-10-09 14:58:44 +0200

    Add statsd metrics for SyncClient HTTPErrors.

--- syncto/views/errors.py
+++ syncto/views/errors.py
@@ -17,39 +17,45 @@ def response_error(context, request):
     message = '%s %s: %s' % (context.response.status_code,
                              context.response.reason,
                              context.response.text)
+
+    statsd = request.registry.statsd
+
+    if statsd:
+        statsd.count("syncclient.status_code.%s" %
+                     context.response.status_code)
+
     # For this specific code we do not want to log the error.
     if context.response.status_code == 304:
         response = httpexceptions.HTTPNotModified()
+    elif context.response.status_code == 400:
+        response = http_error(httpexceptions.HTTPBadRequest(),
+                              errno=ERRORS.INVALID_PARAMETERS,
+                              message=message)
+    elif context.response.status_code == 401:
+        response = http_error(httpexceptions.HTTPUnauthorized(),
+                              errno=ERRORS.INVALID_AUTH_TOKEN,
+                              message=message)
+        # Forget the current user credentials.
+        response.headers.extend(forget(request))
+    elif context.response.status_code == 403:
+        response = http_error(httpexceptions.HTTPForbidden(),
+                              errno=ERRORS.FORBIDDEN,
+                              message=message)
+    elif context.response.status_code == 404:
+        response = http_error(httpexceptions.HTTPNotFound(),
+                              errno=ERRORS.INVALID_RESOURCE_ID,
+                              message=message)
+    elif context.response.status_code == 412:
+        message = 'Resource was modified meanwhile'
+        response = http_error(httpexceptions.HTTPPreconditionFailed(),
+                              errno=ERRORS.MODIFIED_MEANWHILE,
+                              message=message)
     else:
         # For this code we also want to log the error.
         logger.error(context, exc_info=True)
-        if context.response.status_code == 400:
-            response = http_error(httpexceptions.HTTPBadRequest(),
-                                  errno=ERRORS.INVALID_PARAMETERS,
-                                  message=message)
-        elif context.response.status_code == 401:
-            response = http_error(httpexceptions.HTTPUnauthorized(),
-                                  errno=ERRORS.INVALID_AUTH_TOKEN,
-                                  message=message)
-            # Forget the current user credentials.
-            response.headers.extend(forget(request))
-        elif context.response.status_code == 403:
-            response = http_error(httpexceptions.HTTPForbidden(),
-                                  errno=ERRORS.FORBIDDEN,
-                                  message=message)
-        elif context.response.status_code == 404:
-            response = http_error(httpexceptions.HTTPNotFound(),
-                                  errno=ERRORS.INVALID_RESOURCE_ID,
-                                  message=message)
-        elif context.response.status_code == 412:
-            message = 'Resource was modified meanwhile'
-            response = http_error(httpexceptions.HTTPPreconditionFailed(),
-                                  errno=ERRORS.MODIFIED_MEANWHILE,
-                                  message=message)
-        else:
-            response = service_unavailable(
-                httpexceptions.HTTPServiceUnavailable(),
-                request)
+        response = service_unavailable(
+            httpexceptions.HTTPServiceUnavailable(),
+            request)
 
     request.response = response
     export_headers(context.response, request)
