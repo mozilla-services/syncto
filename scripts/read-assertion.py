8727e857b6e5310e7dd2d1687eab3aacae74aa63
Rémy HUBSCHER <rhubscher@mozilla.com>
2016-01-12 11:23:59 +0100

    Display a more lisible timestamp.

--- scripts/read-assertion.py
+++ scripts/read-assertion.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+from __future__ import print_function
 import base64
+import datetime
+import json
 import sys
 
 
@@ -28,5 +31,13 @@ if __name__ == '__main__':
         # Ignore signatures and cryptographic hashes
         if decoded_fragment.startswith('{'):
             print(decoded_fragment)
+            fragment = json.loads(decoded_fragment)
+            for key in ("iat", "exp", "fxa-generation", "fxa-lastAuthAt"):
+                if key in fragment:
+                    timestamp = fragment[key]
+                    if len(str(timestamp)) > 11:
+                        timestamp /= 1e3
+                    date = datetime.datetime.fromtimestamp(timestamp)
+                    print(key, ":", date.strftime("%Y-%m-%d %H:%M:%S"))
 
     print("---- END ASSERTION ----")
